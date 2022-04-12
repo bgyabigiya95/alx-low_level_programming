f9e48798ac38f72f85d25e6879a93fe4cdd733c8
Abigiya Haile <bgyabigiya95@gmail.com>
2022-04-12 09:58:03 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@ void print_times_table(int n)
 {
 int num, mult, prod;
 
-if (n>= 0 && n <= 15; num++)
+if (n >= 0 && n <= 15)
 {
-for (num 0; num <= n; num++)	
+for (num = 0; num <= n; num++)	
 {
 _putchar('0');
 
@@ -23,8 +23,8 @@ _putchar(' ');
 prod = num * mult;
 
 if (prod <= 99)
-_putchar(' '),
-if (prod <= 9);
+_putchar(' ');
+if (prod <= 9)
 _putchar(' ');
 
 if (prod >= 100)
