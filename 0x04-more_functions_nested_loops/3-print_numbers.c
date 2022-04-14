41e4e87bbd16720040edeb0656ebcc799fb664e2
Abigiya Haile <bgyabigiya95@gmail.com>
2022-04-14 11:30:15 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - Prints the number zero to nine
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_number(void)
+{
+
+int i;
+
+i = 0;
+
+for (i = 0; i < 10; i++)
+
+_putchar ('\n');
+
+_putchar ('\n');
+}
