79c1f894c3ab98f0d47d70f6ded62c0000901f99
Abigiya Haile <bgyabigiya95@gmail.com>
2022-04-18 13:43:43 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Takesa pointer to an int and updates 
+ * reset_to_98 - Takes a pointer to an int and updates 
  *               the value it points to to 98.
  * @n: The pointer to an int.
  */
